e412898f3c5c619a7803d9c807b23e0d77dee733
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-07-18 01:34:42 +0200

    mine

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const url = "mongodb://localhost:27017/chat";
+const url = "mongodb+srv://admin1:anime@cluster0.fgqdy.mongodb.net/chatter?retryWrites=true&w=majority";
 
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 
