334753ff0a913cab9fef708ab9746e00a0fa6e46
ayoub-smayen <ayoubjobs.2019@gmail.com>
2021-07-18 01:45:07 +0200

    path

--- app.js
+++ app.js
@@ -22,7 +22,20 @@ app.use("/chats", chatRouter);
 app.use("/login", loginRouter);
 
 //set the express.static middleware
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
+
+const path = require('path');
+//const express = require('express');
+//const app = express();
+const publicPath = path.join(__dirname, '.', 'public');
+//const port = process.env.PORT || 3001;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+/*app.listen(port, () => {
+   console.log('Server is up!');
+});*/
 
 //integrating socketio
 socket = io(http);
